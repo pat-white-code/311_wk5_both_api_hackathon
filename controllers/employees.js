9c92188f16a687055a7813eefc5fbbbb61a149f1
pat-white-code <pat.k.white@gmail.com>
2020-02-04 20:40:29 -0600

    imports getemployeeById

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
 const pool = require('../mysql/connection');
 const getEmployees = require('./getEmployees');
+const getEmployeeById = require('./getEmployeeById');
 
 // Update the getEmployees function so that it calls the database, selecting all fields from the employees table but limiting the results to 50. Use res.json to return the results to the user
 
 module.exports = {
-  getEmployees
+  getEmployees,
+  getEmployeeById
 }
