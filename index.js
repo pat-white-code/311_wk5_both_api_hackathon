a6797487b41f6b2c9215fb7cfb57e38f7d558269
EmiKusaba <eeemi615@gmail.com>
2020-02-04 19:54:10 -0600

    Added Basic Express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('re-testing')
+const express = require('express')
+const app = express()
+// const bodyParser = require("body-parser");
+const port = process.env.PORT || 4001;
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our server!')
+})
+
+app.listen(port, () => {
+  console.log(`Web server is listening on port ${port}!`);
+ });
