8a3f9238c045e42cd3c76ece923e2f7183fbe56b
pat-white-code <pat.k.white@gmail.com>
2020-02-04 20:48:17 -0600

    saves files

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const pool = require('../mysql/connection');
-const getEmployees = require('./getEmployees');
-const getEmployeeById = require('./getEmployeeById');
+const getEmployees = require('./modules/employees/getEmployees');
+const getEmployeeById = require('./modules/employees/getEmployeeById');
 
 // Update the getEmployees function so that it calls the database, selecting all fields from the employees table but limiting the results to 50. Use res.json to return the results to the user
 
--- controllers/modules/employees/getEmployeeById.js
+++ controllers/modules/employees/getEmployeeById.js
@@ -1,4 +1,4 @@
-const pool = require('../mysql/connection');
+const pool = require('../../../mysql/connection');
 const mysql = require('mysql');
 
 // Update the getEmployees function so that it calls the database, selecting all fields from the employees table but limiting the results to 50. Use res.json to return the results to the user
