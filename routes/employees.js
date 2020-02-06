026e9fe9ff87b8ba3a62cc19509fdd336f31e8dd
pat-white-code <pat.k.white@gmail.com>
2020-02-06 09:17:33 -0600

    corrects getEmployeeById route with correct controller

--- routes/employees.js
+++ routes/employees.js
@@ -7,7 +7,7 @@ const controller = require('../controllers/employees');
 // router.get('/, /:id, firstname/:first_name', (req, res) => {
 //   res.send('getting employees...')
 // })
-router.get('/:id', controller.getUserById);
+router.get('/:id', controller.getEmployeeById);
 // router.get('/:first_name', controller.getUserByfirst_name)
 router.get('/', controller.getEmployees);
 // router.put('/:first_name', controller.getByFirstName)
