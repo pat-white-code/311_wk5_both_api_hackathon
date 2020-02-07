e4634c906bfd4c83b9a0bc3efe462c83a04a8175
pat-white-code <pat.k.white@gmail.com>
2020-02-06 20:36:57 -0600

    creating salariesQuery module

--- controllers/modules/salaries/getSalariesByMin.js
+++ controllers/modules/salaries/getSalariesByMin.js
@@ -3,18 +3,28 @@ const mysql = require('mysql');
 
 // Update the getEmployees function so that it calls the database, selecting all fields from the employees table but limiting the results to 50. Use res.json to return the results to the user
 
-const getSalariesByMin = (req, res) => {
+const salariesQuery = (req, res) => {
+  let replacements = [];
+
   let sql = `
   SELECT employees.emp_no, MIN(first_name), MIN(last_name), MAX(from_date), MAX(salary)
   FROM employees 
   JOIN salaries 
     ON salaries.emp_no = employees.emp_no
-  GROUP BY employees.emp_no
-  HAVING MAX(salary) > ?
-  ORDER BY MAX(salary) DESC
-  LIMIT 100;
-  `;
-  let replacements = [req.query.min_sal];
+  GROUP BY employees.emp_no`;
+  // HAVING MAX(salary) > ?;
+  if(req.query.min_sal) {
+    sql += ` HAVING MAX(salary) > ?`
+    replacements.push(req.query.min_sal)
+  }
+
+  if(req.query.max_sal) {
+    sql += ` > ?`
+    replacements.push(req.query.max_sal)
+  }
+
+  sql += `;`
+
   sql = mysql.format(sql, replacements);
 
   pool.query(sql, (err, rows)=> {
@@ -24,4 +34,4 @@ const getSalariesByMin = (req, res) => {
     return res.json(rows) ;
   })}
 
-  module.exports = getSalariesByMin;
+  module.exports = salariesQuery;
